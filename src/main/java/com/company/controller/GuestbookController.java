e53a197fff5e7171c77aecd98b1953d21e5addb1
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-07-02 15:44:31 +0200

    Add setRoutes() to GuestbookServer + structure of Controller

---
+++ src/main/java/com/company/controller/GuestbookController.java
@@ -0,0 +1,14 @@
+package com.company.controller;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+import java.io.IOException;
+
+public class GuestbookController implements HttpHandler {
+
+    @Override
+    public void handle(HttpExchange exchange) throws IOException {
+
+    }
+}
---
+++ src/main/java/com/company/controller/GuestbookServer.java
@@ -0,0 +1,25 @@
+package com.company.controller;
+
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
+
+public class GuestbookServer {
+    private HttpServer server;
+
+    public GuestbookServer() throws IOException {
+        this.server = HttpServer.create(new InetSocketAddress(8001), 0);
+        setRoutes(this.server);
+    }
+
+    private void setRoutes(HttpServer httpServer) {
+        httpServer.createContext("/guestbook", new GuestbookController());
+        httpServer.setExecutor(null);
+    }
+
+    private HttpServer getServer() {
+        return this.server;
+    }
+}
