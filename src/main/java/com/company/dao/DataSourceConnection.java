54ad0ec4c5a489c830792dc66656139bea1f7e80
Bartosz Maleta <bartosz.maleta@gmail.com>
2020-07-02 15:36:18 +0200

    Add dependencies + add DB connector

---
+++ src/main/java/com/company/dao/DataSourceConnection.java
@@ -0,0 +1,32 @@
+package com.company.dao;
+
+import org.apache.commons.dbcp2.BasicDataSource;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public class DataSourceConnection {
+    private static BasicDataSource basicDataSource;
+
+    public DataSourceConnection() {
+        basicDataSource = new BasicDataSource();
+
+        basicDataSource.setUrl("jdbc:postgresql://localhost:5432/guestbook");
+        basicDataSource.setUsername("postgres");
+        basicDataSource.setPassword("admin");
+        basicDataSource.setMinIdle(5);
+        basicDataSource.setMaxIdle(10);
+        basicDataSource.setMaxOpenPreparedStatements(100);
+    }
+
+    public Connection getConnection() {
+        Connection connection = null;
+
+        try {
+            connection = basicDataSource.getConnection();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connection;
+    }
+}
